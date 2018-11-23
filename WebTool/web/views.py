5bd09dd56e3ed52027e418c4777cd8d52e0b84b7
HaobingLiu <liuhaobing@sjtu.edu.cn>
2018-11-23 14:08:06 +0800

    liu did

--- WebTool/web/views.py
+++ WebTool/web/views.py
@@ -6,3 +6,12 @@ from django.shortcuts import render_to_response
 # Create your views here.
 def homepage(request):
     return render_to_response('servermaterial/index.html')
+
+def monitor_all(request):
+    return render_to_response('servermaterial/monitor_all.html')
+
+def study_well(request):
+    return render_to_response('servermaterial/study_well.html')
+
+def study_poor(request):
+    return render_to_response('servermaterial/study_poor.html')
