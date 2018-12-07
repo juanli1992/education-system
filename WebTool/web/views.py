0663a59b445d1e9dc12e99890bc38e3360cde385
HaobingLiu <liuhaobing@sjtu.edu.cn>
2018-12-07 10:54:51 +0800

    liu did

--- WebTool/web/views.py
+++ WebTool/web/views.py
@@ -15,3 +15,6 @@ def study_well(request):
 
 def study_poor(request):
     return render_to_response('servermaterial/study_poor.html')
+
+def first(request):
+    return render_to_response('servermaterial/first.html')
